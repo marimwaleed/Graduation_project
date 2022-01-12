5d913a6e0c4e0427426071dbb3678c99a7979267
Mariam <marimwaleed03@gmail.com>
2022-01-12 14:10:12 +0200

    adding project

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template, Markup
+
+from sentiment_analyzer.model import Model
+
+
+
+model = Model()
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['POST', 'GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST', 'GET'])
+def pred():
+    sentiment, confidence = model.predict(request.form['Sentence'])
+
+    if sentiment == 'Bad':
+        sentiment = 'Bad Review 😒😒'
+        
+    elif sentiment == 'Wonderfull':
+        sentiment = 'Wonderfull Review 😊😊'
+        
+    else:
+        sentiment = 'Normal Review 😐😐'
+        
+
+
+    return render_template('predict.html',
+                            sentiment= sentiment,
+                            confidence = f'Confidence = {confidence} %')
+    
+if __name__ == "__main__":
+    app.run()
+   
